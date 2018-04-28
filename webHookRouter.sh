8af81c684d6f5de628ba062cae352fff59b0a6f5
zxldev <zxldev@live.com>
2018-04-28 12:06:22 +0800

    添加配置

--- webHookRouter.sh
+++ webHookRouter.sh
@@ -3,6 +3,6 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 echo "$*" >> ${SHELL_FOLDER}/route.log
 projectName="$1"
 
-if [ -f "${SHELL_FOLDER}/${projectName}.sh" ];then
-  $("${SHELL_FOLDER}/hooks/${projectName}.sh")
+if [ -f "${SHELL_FOLDER}/hooks/${projectName}.sh" ];then
+  /bin/sh "${SHELL_FOLDER}/hooks/${projectName}.sh"
 fi
